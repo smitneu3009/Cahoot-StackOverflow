cdeb4c52973d32dfa5d45e06e2c78a1a0da60899
Smit Patel <patel.smit5@northeastern.edu>
2024-02-06 00:27:57 -0500

    Initial commit

---
+++ StackOverflow/StackOverflowProject.ViewModels/PostDetailViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace StackOverflowProject.ViewModels
+{
+    public class PostDetailViewModel
+    {
+        public string PostType { get; set; } // Type of post: "Question" or "Answer"
+        public string DayOfWeek { get; set; } // Day of the week
+        public int TotalPosts { get; set; } // Total number of posts
+        public int TotalUpvotes { get; set; } // Total number of upvotes
+        public int TotalDownvotes { get; set; } // Total number of downvotes
+        public double AvgUpvoteDownvoteRatio { get; set; } // Average upvote to downvote ratio
+
+        // Constructor
+        public PostDetailViewModel(string postType, string dayOfWeek, int totalPosts, int totalUpvotes, int totalDownvotes, double avgUpvoteDownvoteRatio)
+        {
+            PostType = postType;
+            DayOfWeek = dayOfWeek;
+            TotalPosts = totalPosts;
+            TotalUpvotes = totalUpvotes;
+            TotalDownvotes = totalDownvotes;
+            AvgUpvoteDownvoteRatio = avgUpvoteDownvoteRatio;
+        }
+    }
+}
